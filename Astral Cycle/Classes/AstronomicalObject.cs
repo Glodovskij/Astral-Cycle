51c7cf2b5bb71cf69e99dbb1696cefeb31e1fad9
Viktor Kryvoruchko <deadprome@gmail.com>
2019-09-15 18:05:47 +0300

    Added AstralObject + Star

---
+++ Astral Cycle/Classes/AstronomicalObject.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TestConsole
+{
+    public abstract class AstronomicalObject
+    {
+        public string Name { get; set; }
+        public double Radius { get; set; }
+        public double Weight { get; set; }
+        public double Temperature { get; set; }
+
+        public Dictionary<string, double> Composition = new Dictionary<string, double>();
+
+        public double GetDiameter()
+        {
+            return this.Radius * 2;
+        }
+        public double GetEquatorLength()
+        {
+            return 2 * this.Radius * Math.PI;
+        }
+        public void GetAxisRotationSpeed()
+        {
+            return;
+        }
+        public void GetOrbitRotationSpeed()
+        {
+            return;
+        }
+
+        public AstronomicalObject(string name, double radius, double weight, double temperature)
+        {
+            Name = name;
+            Radius = radius;
+            Weight = weight;
+            Temperature = temperature;
+        }
+    }
+}
---
+++ Astral Cycle/Classes/Star.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestConsole
+{
+    public enum StarType
+    {
+        Blue,
+        Yellow,
+        Red
+    }
+
+    public struct StarCharacteristics
+    {
+        const double KelvinCelsiusDifference = 273.15;
+        private StarType type;
+
+        public StarType Type
+        {
+            get { return type; }
+            set { type = value; }
+        }
+        
+        public double KelvinTemperature
+        {
+            get
+            {
+                double temperatureValue = 0;
+                Random rnd = new Random();
+                switch(this.Type)
+                {
+                    case StarType.Blue:
+                        temperatureValue = rnd.Next(7500, 10000);
+                        break;
+                    case StarType.Yellow:
+                        temperatureValue = rnd.Next(5000, 7499);
+                        break;
+                    case StarType.Red:
+                        temperatureValue = rnd.Next(2000, 4999);
+                        break;
+                }
+                return temperatureValue;
+            }
+        }
+
+        public double CelsiusTemperature
+        {
+            get { return KelvinTemperature - KelvinCelsiusDifference; }
+        }
+
+        public double FarenheitTemperature
+        {
+            get { return (KelvinTemperature - KelvinCelsiusDifference) * 9/5 + 32; }
+        }
+
+        public StarCharacteristics(StarType type)
+        {
+            this.type = type;
+        }
+    }
+
+
+
+
+    public class Star: AstronomicalObject
+    {
+        public StarCharacteristics Characteristics;
+
+        public Star(string name, double radius, double weight, StarCharacteristics characteristics)
+            :base(name,radius,weight, characteristics.KelvinTemperature)
+        {
+            Characteristics = characteristics;
+        }
+    }
+}
