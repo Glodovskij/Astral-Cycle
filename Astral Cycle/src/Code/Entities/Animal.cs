7b9b940b7f9938634b12eb09e2bce69b7b3db864
Aleksei Glodovskij <glodovskii@gmail.com>
2019-09-19 11:10:45 +0300

    Added File Planet, edited file Animal

--- Astral Cycle/src/Code/Entities/Animal.cs
+++ Astral Cycle/src/Code/Entities/Animal.cs
@@ -9,7 +9,9 @@ namespace Astral_Cycle.src.Code.Entities
 {
     enum AnimalsType
     {
-       /*травоядный*/ Herbivorous, Predatory
+       /*травоядный*/ Herbivorous,
+        Predatory,
+        Omnivores //Всеядные
     }
     class Animal : Organism
     {
@@ -26,6 +28,7 @@ namespace Astral_Cycle.src.Code.Entities
             //Animan eats another animal with 50 % chanse if:
             //  1.Predator eats herbivorous
             //  2.Predator eats another predator if victim is another kind of predator
+            //  3. Omnivores eats all but not own 
             // returning value: 
             // true - eats
             // false - not
@@ -50,14 +53,24 @@ namespace Astral_Cycle.src.Code.Entities
                         }
                 }
             }
+            if(AnimalType.Equals(AnimalsType.Omnivores))
+            {
+                if (animal.Name != Name)
+                    if (rand.Next(1, 2) == 1)
+                    {
+                        HPTicks = 2;
+                        return true;
+                    }
+            }
             return false;
         }
         public bool Eat(Plant plant)
         {
             Random rand = new Random();
 
-            if (rand.Next(1, 2) == 1)
-                return true;
+            if(AnimalType.Equals(AnimalsType.Omnivores) || AnimalType.Equals(AnimalsType.Herbivorous))
+                if (rand.Next(1, 2) == 1)
+                    return true;
 
             return false;
         }
---
+++ Astral Cycle/src/Code/Entities/Planet.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Astral_Cycle.src.Code.Entities
+{
+    enum PlanentsType
+    {
+        EarthLike, //Подобные земле
+        GiantLike, //Планеты гиганты
+        DwarfLike //Карлики
+    }
+    class Planet : AstronomicalObject
+    {
+        public PlanentsType PlanentsType { get; set; }
+
+        private Dictionary<string, float> atmosphere = new Dictionary<string, float>();
+
+        public bool IsHabitable { get; set; }
+
+        public List<Animal> Herbivorous { get; set; } = new List<Animal>();
+        public List<Animal> Omnivores { get; set; } = new List<Animal>();
+        public List<Animal> Predatory { get; set; } = new List<Animal>();
+        public List<Plant> Plants { get; set; } = new List<Plant>();
+
+        public Planet(string name, double radius, double weight, double temperature, Point position, 
+            bool isHabitable, Dictionary<string, float> atmosphere, List<Animal> herbivorous,
+            List<Animal> omnivores, List<Animal> predatory, List<Plant> plants)
+            :base(name, radius, weight, temperature, position)
+        {
+            Herbivorous = herbivorous;
+            Omnivores = omnivores;
+            Predatory = predatory;
+            Plants = plants;
+
+            IsHabitable = isHabitable;
+
+            this.atmosphere = atmosphere;
+        }
+
+    }
+}
